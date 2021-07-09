2c85125d2f2fd96839749acfc6fd43f7910b60cf
rsrajput <rsrajput@gmail.com>
2021-07-09 12:02:36 +0530

    Backup and then conditional deletion of init.vim on successful download from github

--- git_init.vim_downloader.sh
+++ git_init.vim_downloader.sh
@@ -1,11 +1,18 @@
  #!/bin/bash
 
  cd /home/rsrajput/.config/nvim/ 
- rm init.vim
+ mv init.vim init.vim.backup # backup existing file before download or deleting.
  
- # wget https://raw.githubusercontent.com/rsrajput/dotfiles-universal/main/init.vim
- 
+# download latest init.vim from github.
+
  curl -H 'Authorization: token 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' \
   -H 'Accept: application/vnd.github.v3.raw' \
   -O \
   -L https://raw.githubusercontent.com/rsrajput/dotfiles-universal/main/init.vim
+
+ echo "init.vim successfully updated from Github repo."
+
+# delete backup file if curl successfully downloaded init.vim from github.
+if [ -f init.vim ] ; then
+    rm init.vim.backup
+fi
